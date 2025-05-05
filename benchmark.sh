ba846374cb0c7194f72b06dab61b97b2c30e164e
PathToLife <12622625+PathToLife@users.noreply.github.com>
2025-05-05 23:32:24 +1200

    refactor. improve benchmark.sh UX. refactor variable parameterisation

--- benchmark.sh
+++ benchmark.sh
@@ -28,24 +28,35 @@ else
   exit 1
 fi
 
+# Check if server is running
+response=$(curl -s -o /dev/null --max-time 1 -w "%{http_code}" "$BASEURL")
+
+# Allow 200 or 404 as valid responses
+if [ "$response" -ne 200 ] && [ "$response" -ne 404 ]; then
+  echo "Error: Server at $BASEURL took too long to respond. Have we started the server? (pnpm start)?"
+  exit 1
+fi
+
 # Functions for benchmark tests
+
 parallel_api_call_sim() {
-  time curl "${BASEURL}/hello" --max-time 1 --silent --show-error --max-time 1 --fail -o /dev/null
+  # Sends an GET request to /hello with a 1 sec timeout
+  time curl "$BASEURL/hello" --max-time 1 --silent --show-error --max-time 1 --fail -o /dev/null
   if [ $? -eq 0 ]; then
-      echo "Parallel Curl ${BASEURL}/hello succeeded!"
+      echo "Parallel Curl $BASEURL/hello succeeded!"
   else
-      echo "Parallel Curl ${BASEURL}/hello failed. This means server has performance issues"
+      echo "Parallel Curl $BASEURL/hello failed. This means server has performance issues"
   fi
 }
 
 run_worker_test() {
-  wrk -t12 "-c${CONCURRENT_REQUESTS}" -d10s "$BASEURL/prime/$MODE" &
+  wrk -t12 "-c$CONCURRENT_REQUESTS" -d10s "$BASEURL/prime/worker" &
   sleep 2s;
   parallel_api_call_sim
 }
 
 run_single_test() {
-  wrk -t12 "-c${CONCURRENT_REQUESTS}" -d10s "$BASEURL/prime/$MODE" &
+  wrk -t12 "-c$CONCURRENT_REQUESTS" -d10s "$BASEURL/prime/single" &
   sleep 2s;
   parallel_api_call_sim
 }
